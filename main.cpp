d137780318ca0bb4301851d230dc93b271a63d79
Adrien-Meilac <abonnements.ameilac@outlook.fr>
2017-12-10 14:03:26 +0100

    add statset

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "PKMN_Tools/PKMN_Conversion.h"
+#include "PKMN_Tools/PKMN_StatSet.h"
 
 using namespace std;
 
@@ -8,5 +9,7 @@ int main()
     cout << "Hello world!" << endl;
     int a = string_to_int("1232");
     cout << a;
+    PKMN_StatSet A;
+    cout << A;
     return 0;
 }
