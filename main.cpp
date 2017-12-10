c4b39d22406c35a6bc2cde2e11a54fa22a40e6ff
Adrien-Meilac <abonnements.ameilac@outlook.fr>
2017-12-10 13:57:41 +0100

    test conversion

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
+#include "PKMN_Tools/PKMN_Conversion.h"
 
 using namespace std;
 
 int main()
 {
     cout << "Hello world!" << endl;
+    int a = string_to_int("1232");
+    cout << a;
     return 0;
 }
